55844145b5926839932db803bc58fa887550f570
Andrea G. Aristegui <andrearistegui@gmail.com>
2020-12-07 11:48:31 +0100

    Problem with initiallyLoggedUser

--- src/components/login/PrivateRoute.js
+++ src/components/login/PrivateRoute.js
@@ -1,17 +1,11 @@
 import React from 'react';
-import { Route, Redirect, useLocation } from 'react-router-dom';
+import { Route, Redirect } from 'react-router-dom';
 
 function PrivateRoute(props) {
   console.log('props en PrivateRoute:', props);
   const { isLogged } = props;
   console.log('isLogged en PrivateRoute:', isLogged);
-  const location = useLocation();
-  console.log('location:', location);
-  return isLogged ? (
-    <Route {...props} />
-  ) : (
-    <Redirect to={{ pathname: '/login', state: { from: location } }} />
-  );
+  return isLogged ? <Route {...props} /> : <Redirect to="/login" />;
 }
 
 export default PrivateRoute;
